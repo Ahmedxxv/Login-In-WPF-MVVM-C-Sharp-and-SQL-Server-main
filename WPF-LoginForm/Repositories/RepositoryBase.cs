6e7cb96e73045b9916adea60c77007204cda4157
Ahmedxxv <121236178+Ahmedxxv@users.noreply.github.com>
2023-10-06 16:00:27 +0200

    Initial commit

---
+++ WPF-LoginForm/Repositories/RepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WPF_LoginForm.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            // _connectionString = "Server=(local); Database=MVVMLoginDb; Integrated Security=true";
+
+            _connectionString = $"Server=0000;Database=0000;User Id=0000;Password=0000;";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
